62e4a8b7c6dd53c977b5c31306b7437a03930778
zihao.liu <zihao.liu@asiainnovations.com>
2018-09-18 13:29:56 +0800

    multi lighting

--- app/src/main/assets/point/vertex_shader.glsl
+++ app/src/main/assets/point/vertex_shader.glsl
@@ -1,8 +1,12 @@
 uniform mat4 uMVPMatrix;
 attribute vec4 aPosition;
 
-void main()                    
-{                              
+attribute vec4 aColor;
+
+varying vec4 vColor;
+void main()
+{
+    vColor = aColor;
 	gl_Position = uMVPMatrix * aPosition;
-    gl_PointSize = 5.0;         
-}                              
+    gl_PointSize = 5.0;
+}
